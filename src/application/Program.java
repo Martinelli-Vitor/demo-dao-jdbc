0947129e468b653be1e2ee4b82553b2235321bc2
Martinelli-Vitor <vitormartinellilobo@gmail.com>
2021-09-01 19:59:32 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 		
 		Seller sel = new Seller(1,"Bob","bob@gmail.com",new Date(),4000.0,ob);
 		System.out.println(sel);
+		
+		SellerDao sellerDao = DaoFactory.ceateSellerDao();
 
 	}
 
